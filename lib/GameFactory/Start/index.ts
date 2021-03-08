c469b3cfe824f2f0d9e9f40016f387d87651ae81
Karel Nguyen <karel.nguyen@qest.cz>
2021-03-08 11:22:13 +0100

    fix(empty board)

--- lib/GameFactory/Start/index.ts
+++ lib/GameFactory/Start/index.ts
@@ -4,9 +4,9 @@ import { Board, Game, GameStatus } from '../types';
 export const startGame = (
   board: Board = [
     [0, 0, 0, 0],
-    [1024, 32, 0, 0],
-    [0, 64, 0, 0],
-    [0, 0, 0, 2048],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
   ]
 ): Game => ({
   board: getDefaultBoard(board),
